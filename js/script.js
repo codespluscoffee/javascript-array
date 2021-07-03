0b91a92a572ae5b0655e4fb80a2f9418ee128c55
arodriguez2323 <rodriguezarnaldo81@gmail.com>
2021-07-03 09:57:05 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+//Declare Array
+const colors = ["red", "orange", "purple"];
+
+// Convert Array to String
+const colors = ["red", "orange", "purple"];
+console.log(colors.toString());
+
+//result "red,orange,purple"
+
+// Join Array
+const colors = ["red", "orange", "purple"];
+console.log(colors.join(" * "));
+
+//result "red * orange * purple"
+
+// Popping
+const colors = ["red", "orange", "purple"];
+let x = colors.pop();
+
+//result ["red", "orange"]
+
+//Pushing 
+const colors = ["red", "orange", "purple"];
+colors.push("green");
+
+//result ["red", "orange", "purple", "green"]
+
+// Shifting Elements
+const colors = ["red", "orange", "purple"];
+let x = colors.shift();    // x = "red" 
+
+
+//Unshift Elements
+const colors = ["red", "orange", "purple"];
+colors.unshift("green");   //["green","red", "orange", "purple"]
+
+
+
+
